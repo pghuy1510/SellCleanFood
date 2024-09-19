db3268311ac27eabf4d5ddcdb25b32beaddeb497
Phạm Gia huy <121171972+pghuy1510@users.noreply.github.com>
2024-09-19 21:43:58 +0700

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Sell_Clean_Food.repository;
-
-import com.example.Sell_Clean_Food.security.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  User findByEmail(String email);
-  boolean existsByEmail(String email);
-}
